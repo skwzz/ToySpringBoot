93d470b3173b1545a3c37eabd2ac3738045149c9
KiwoongSon <skw9108@ibizsoftware.net>
2022-06-07 14:33:16 +0900

    Initial commit

---
+++ src/main/java/com/skwzz/toyspringboot/ToySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.skwzz.toyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skwzz/toyspringboot/ToySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skwzz.toyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
