87936cd8c0086916bedd30d70aa25229f00f2bb0
KiwoongSon <skw9108@ibizsoftware.net>
2022-06-08 17:20:32 +0900

    s3 mock 설정중

--- src/main/java/com/skwzz/toyspringboot/config/S3MockConfig.java
+++ src/main/java/com/skwzz/toyspringboot/config/S3MockConfig.java
@@ -1,4 +1,74 @@
 package com.skwzz.toyspringboot.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import io.findify.s3mock.S3Mock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+@Configuration
 public class S3MockConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.mock.port}")
+    private int port;
+
+    @Bean
+    public S3Mock s3Mock(){
+        return new S3Mock.Builder()
+                .withPort(port)
+                .withInMemoryBackend()
+                .build();
+    }
+
+    @PostConstruct
+    public void startS3Mock(){
+        this.s3Mock().start();
+        log.info(" ----- Started in-memory S3 Mock -----");
+    }
+
+    @PreDestroy
+    public void destoryS3Mock(){
+        this.s3Mock().shutdown();
+        log.info(" ----- Shut downed in-memory S3 Mock -----");
+    }
+
+    @Bean
+    @Primary
+    public AmazonS3 amazonS3Client(){
+        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(getUri(), region);
+        AmazonS3 client = AmazonS3ClientBuilder
+                .standard()
+                .withPathStyleAccessEnabled(true)
+                .withEndpointConfiguration(endpoint)
+                .withCredentials(new AWSStaticCredentialsProvider(new AnonymousAWSCredentials()))
+                .build();
+        client.createBucket(bucket);
+        return client;
+    }
+
+    private String getUri(){
+        return UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(port)
+                .build()
+                .toUriString();
+    }
 }
